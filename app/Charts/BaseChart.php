72c841d7493a67b017f99d88069f49353c95422d
Kaishiyoku <dmp@nfze.de>
2019-01-02 16:28:45 +0100

    fixed import

--- app/Charts/BaseChart.php
+++ app/Charts/BaseChart.php
@@ -2,7 +2,7 @@
 
 namespace App\Charts;
 
-use ConsoleTVs\Charts\Classes\ChartJs\Chart;
+use ConsoleTVs\Charts\Classes\Chartjs\Chart;
 use ConsoleTVs\Charts\Classes\DatasetClass;
 use Illuminate\Support\Facades\View;
 
