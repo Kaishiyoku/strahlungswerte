09246d195a4a44c9ba21d226a1a80432a01e662d
Kaishiyoku <dmp@nfze.de>
2021-08-17 23:05:34 +0200

    optimized CSS

--- resources/views/shared/_odl_copyright_notice.blade.php
+++ resources/views/shared/_odl_copyright_notice.blade.php
@@ -1,3 +1,3 @@
-<div class="alert alert-info">
+<div class="alert alert--info">
     {!! __('location.copyright_notice', ['link' => html()->a('https://odlinfo.bfs.de')->class('alert-link')]) !!}
 </div>
