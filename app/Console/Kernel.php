1b85cbeaeed965fb248da580d2cfdd54a5d5d307
Kaishiyoku <dmp@nfze.de>
2022-04-15 14:20:06 +0200

    added laravel-backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        $schedule->command('backup:run')->daily()->at('01:30');
+
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
 
         $schedule->command(OdlUpdateLocations::class)->hourlyAt(30);
